65854d4fc62150ee6535a8d948c9622cbf056fae
Adam Wilder <wilderav86@gmail.com>
2022-03-14 12:37:48 -0700

    installed gatsby plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
+    `gatsby-plugin-netlify`,
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
