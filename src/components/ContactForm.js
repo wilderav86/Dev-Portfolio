90f33846e2107b8b03597cb624e904a82cae4276
Adam Wilder <wilderav86@gmail.com>
2022-06-15 17:48:54 -0700

    added netlify forms support to contact form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,7 +12,12 @@ const ContactForm = () => {
     <div className="form-container">
       <h1 className="form-title page-title accent-color">{formTitle}</h1>
 
-      <form className="ui-form" name="contact" method="post">
+      <form
+        className="ui-form"
+        name="contact"
+        method="post"
+        data-netlify="true"
+      >
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label
