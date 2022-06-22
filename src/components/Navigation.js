cf9d334482bf0b33dbdd9dd2977ee9c8673cafa6
Adam Wilder <wilderav86@gmail.com>
2022-06-21 20:22:24 -0700

    tightened up margins

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { useStaticQuery, graphql } from "gatsby";
 import Toggle from "./Toggle";
 
 import "./Nav.Style.css";
+import FadeInWhenVisible from "../animations/FadeInWhenVisible";
 
 const Navigation = () => {
   const data = useStaticQuery(graphql`
@@ -20,10 +21,24 @@ const Navigation = () => {
     }
   `);
 
-  const { subPages } = data.site.siteMetadata;
-
+  //navbar state
+  const [expanded, setExpanded] = useState(false);
   const [lightTheme, setLightTheme] = useContext(ThemeContext);
 
+  //functions
+  const setToggle = () => {
+    console.log("toggled");
+    setExpanded(true);
+  };
+
+  const setClose = () => {
+    setExpanded(false);
+  };
+
+  //data
+  const { subPages } = data.site.siteMetadata;
+
+  //render variables
   const message = lightTheme ? "lighter" : "darker";
 
   const renderSubPages = subPages.map((subPage) => {
@@ -36,73 +51,119 @@ const Navigation = () => {
     );
   });
 
+  //
+
   return (
-    <div className="navbar-container">
-      <div>
-        <Navbar
-          collapseOnSelect
-          bg="none"
-          variant={lightTheme ? "dark" : "light"}
-          expand="md"
-          fixed="top"
-        >
-          <Container fluid className="nav-container">
-            {/* <div className="nav-container">
-              <div className="drop-down"> */}
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-                {renderSubPages}
-              </Nav>
-              <Navbar.Text>
-                <div className="toggle-text-container">
-                  <div className="chunk-container">
-                    <p
-                      className={
-                        lightTheme ? "text-chunk light" : "text-chunk dark"
-                      }
-                    >
-                      for a
-                    </p>
-                    <p
-                      className={
-                        lightTheme
-                          ? "text-chunk light color"
-                          : "text-chunk dark color"
-                      }
-                    >
-                      <strong>{`${message}`}</strong>
-                    </p>
-                    <p
-                      className={
-                        lightTheme ? "text-chunk light" : "text-chunk dark"
-                      }
-                    >
-                      mood, press
-                    </p>
-                  </div>
+    <Navbar
+      collapseOnSelect
+      bg={lightTheme ? "dark" : "light"}
+      variant={lightTheme ? "dark" : "light"}
+      expand="md"
+      fixed="top"
+    >
+      <Container fluid className="nav-container">
+        {/* <div className="nav-container">
+            <div className="drop-down"> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+            {renderSubPages}
+          </Nav>
+          <Navbar.Text>
+            <div className="toggle-text-container">
+              <div className="chunk-container">
+                <p
+                  className={
+                    lightTheme ? "text-chunk light" : "text-chunk dark"
+                  }
+                >
+                  for a
+                </p>
+                <p
+                  className={
+                    lightTheme
+                      ? "text-chunk light color"
+                      : "text-chunk dark color"
+                  }
+                >
+                  <strong>{`${message}`}</strong>
+                </p>
+                <p
+                  className={
+                    lightTheme ? "text-chunk light" : "text-chunk dark"
+                  }
+                >
+                  mood, press
+                </p>
+              </div>
 
-                  <Toggle />
-                </div>
-              </Navbar.Text>
-            </Navbar.Collapse>
-            {/* </div>
-            </div> */}
-          </Container>
-        </Navbar>
-      </div>
+              <Toggle />
+            </div>
+          </Navbar.Text>
+        </Navbar.Collapse>
+        {/* </div>
+          </div> */}
+      </Container>
+    </Navbar>
 
-      {/* <div>
-        <Navbar
-          bg="none"
-          variant="dark"
-          fixed="top"
-          className="justify-content-end"
-        >
-          <Toggle />
-        </Navbar>
-      </div> */}
-    </div>
+    // <div className="navbar-container">
+    //   <Navbar
+    //     id="navbar"
+    //     collapseOnSelect
+    //     bg="none"
+    //     variant={lightTheme ? "dark" : "light"}
+    //     expand="md"
+    //     fixed="top"
+    //     // onToggle={setToggle}
+    //   >
+    //     <Container fluid className="nav-container">
+    //       {/* <div className="nav-container">
+    //           <div className="drop-down"> */}
+    //       <Navbar.Toggle aria-controls="navbarScroll" id="toggle" />
+
+    //       <Navbar.Collapse id="navbarScroll" className="collapse">
+    //         {/* {expanded && ( */}
+    //         <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+    //           {renderSubPages}
+    //           <Navbar.Text>
+    //             <div className="toggle-text-container">
+    //               <div className="chunk-container">
+    //                 <p
+    //                   className={
+    //                     lightTheme ? "text-chunk light" : "text-chunk dark"
+    //                   }
+    //                 >
+    //                   for a
+    //                 </p>
+    //                 <p
+    //                   className={
+    //                     lightTheme
+    //                       ? "text-chunk light color"
+    //                       : "text-chunk dark color"
+    //                   }
+    //                 >
+    //                   <strong>{`${message}`}</strong>
+    //                 </p>
+    //                 <p
+    //                   className={
+    //                     lightTheme ? "text-chunk light" : "text-chunk dark"
+    //                   }
+    //                 >
+    //                   mood, press
+    //                 </p>
+    //               </div>
+
+    //               <Toggle />
+    //             </div>
+    //           </Navbar.Text>
+    //         </Nav>
+    //       </Navbar.Collapse>
+
+    //       {/* </div>
+    //         </div> */}
+    //     </Container>
+    //   </Navbar>
+    // </div>
   );
 };
 
